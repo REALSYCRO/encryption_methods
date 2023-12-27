6f5a1cc4a7cc9a5ca500a67a2772b7390f8a2d3e
Sycro <jesperlabudde@gmail.com>
2023-12-27 22:59:58 +0100

    ignore this file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encryption_methods',
+    version='0.1.0',
+    author='Sycro',
+    author_email='arceusstriker@gmail.com',
+    description='A collection of different encryptions',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12'
+)
